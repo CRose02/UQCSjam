e073ba629cacc15bb9375e2c734398e04206a083
CRose02 <130022292+CRose02@users.noreply.github.com>
2026-05-09 08:41:21 +1000

    Player becomes invulnerable and flashes on damage

--- JamDungeon/Assets/Scripts/PlayerDamage.cs
+++ JamDungeon/Assets/Scripts/PlayerDamage.cs
@@ -5,16 +5,54 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
+
+    public Collider2D hurtbox;
+    public SpriteRenderer spriteRenderer;
+
+    private bool isInvinsible = false;
+    public float invinsibleCooldown;
+    private float invinsibleTimer = 0f;
+    public float flashFrequency;
+    public Color flashColor;
+    private Color normalColor;
+    public AnimationCurve flashColorCurve;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        normalColor = spriteRenderer.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInvinsible)
+        {
+            DoInvinsible();
+            DoFlashing();
+        }
+    }
+
+    private void BeginInvinsible()
+    {
+        isInvinsible = true;
+        hurtbox.enabled = false;
+    }
+
+    private void DoInvinsible()
+    {
+        invinsibleTimer += Time.deltaTime;
+        if (invinsibleTimer > invinsibleCooldown)
+        {
+            isInvinsible = false;
+            invinsibleTimer = 0f;
+            hurtbox.enabled = true;
+        }
+    }
+
+    private void DoFlashing()
+    {
+        spriteRenderer.color = Color.Lerp(normalColor, flashColor, flashColorCurve.Evaluate(invinsibleTimer / flashFrequency));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,6 +62,7 @@ public class PlayerDamage : MonoBehaviour
             return;
         }
 
+        BeginInvinsible();
         GameManager.Instance.TakeDamage();
 
         Destroy(collision.gameObject.transform.parent.gameObject);
