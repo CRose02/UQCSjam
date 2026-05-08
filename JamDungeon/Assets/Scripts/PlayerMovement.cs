0e6fa6603813b817d2652b4bb62e4f61bde4bc17
CRose02 <130022292+CRose02@users.noreply.github.com>
2026-05-08 19:18:10 +1000

    Basic player movement

---
+++ JamDungeon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float maxSpeed;
+
+    private Rigidbody2D rb;
+    private float moveForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+    }
+
+    Vector3 move;
+    private void HandleInputs()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        move = new Vector3(x, y, 0f).normalized;
+        moveForce = Mathf.Clamp(maxSpeed - rb.velocity.magnitude, 0f, 100f);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(move * moveForce);
+    }
+}
